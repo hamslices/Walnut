0b4482de851f71b03a60ab39a10551e9f46ae525
hamslices <223341941+hamslices@users.noreply.github.com>
2025-10-06 15:50:06 -0400

    setting up project

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,10 @@
 -- premake5.lua
-workspace "WalnutApp"
+workspace "LarkGUI"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "WalnutApp"
+   startproject "LarkGUI"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 include "WalnutExternal.lua"
-include "WalnutApp"
+include "LarkGUI"
